9da09c1df43b6413687e167562833ce6d9975bfa
takuto.fly <76800704+takutonkatsu@users.noreply.github.com>
2025-06-01 14:53:03 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -12,49 +12,50 @@ if(date_key != ymd){
 //Single5000点 裏世界入口
 myrecord = localStorage.getItem("my_1record");
 if(myrecord==5000){
-    document.getElementById("another_world_entrance").innerHTML = "</center><a href=\"fJpMDEXFiMwi5K3BeAWDsOvWGeNQ7X4lSHYOUf3eNhViTPUPgFVQXY4qoyEJYTYKEGQy0hC8XNptFRRVi8ShvneDkxMtMJaqHHNa.html\" class=\"btn-square btn1\" style=\"background-color: #000000; color: #ffffff;\">　裏世界　</a>" 
+    document.getElementById("another_world_entrance").innerHTML = "</center><a href=\"fJpMDEXFiMwi5K3BeAWDsOvWGeNQ7X4lSHYOUf3eNhViTPUPgFVQXY4qoyEJYTYKEGQy0hC8XNptFRRVi8ShvneDkxMtMJaqHHNa.html\" class=\"btn-square btn1\" style=\"background-color: #000000; color: #ffffff;\">裏世界</a>" 
     
 }
 
 //AO5 4950点以上バッジ
-// Ao5record = localStorage.getItem("my_ao5record");
-// if(Ao5record>=4990){
-//     document.getElementById("title_image").src="img/RgbGuessrGod.jpg"
+Ao5record = localStorage.getItem("my_ao5record");
+if(Ao5record>=4990){
+    document.getElementById("title_image").src="img/RgbGuessrGod.jpg"
 
-// }else if(Ao5record>=4950){
-//     document.getElementById("title_image").src="img/RgbGuessrPro.jpg"
-// } 
+}else if(Ao5record>=4950){
+    document.getElementById("title_image").src="img/RgbGuessrPro.jpg"
+} 
 
 // PB表示
-myrecord = localStorage.getItem("my_1record");
-if(myrecord==null){
 
-}else{
-    document.getElementById("my_1record").innerText = "Single:"+myrecord+"点  "
-}
+// myrecord = localStorage.getItem("my_1record");
+// if(myrecord==null){
+
+// }else{
+//     document.getElementById("my_1record").innerText = "Single:"+myrecord+"点  "
+// }
 
 Ao5record = localStorage.getItem("my_ao5record");
 if(Ao5record==""||Ao5record==null){
     
 }else{
-    document.getElementById("my_ao5record").innerText = "Ao5:"+Ao5record+"点";
+    document.getElementById("my_ao5record").innerText = Ao5record+"点";
 }
 
-myrecord2 = localStorage.getItem("2my_1record");
-if(myrecord2==null){
+// myrecord2 = localStorage.getItem("2my_1record");
+// if(myrecord2==null){
 
-}else{
-    document.getElementById("2my_1record").innerText = "Single:"+myrecord2+"点  "
-}
+// }else{
+//     document.getElementById("2my_1record").innerText = "Single:"+myrecord2+"点  "
+// }
 
 Ao5record2 = localStorage.getItem("2my_ao5record");
 if(Ao5record2==""||Ao5record2==null){
     
 }else{
-    document.getElementById("2my_ao5record").innerText = "Ao5:"+Ao5record2+"点";
+    document.getElementById("2my_ao5record").innerText = Ao5record2+"点";
 }
 
 stage_record = localStorage.getItem("4stage_record");
 if(stage_record!=null){
-    document.getElementById("stage_record").innerText = "ステージ"+stage_record
+    document.getElementById("stage_record").innerText ="ステージ"+stage_record;
 }
